4e32410c1bcfc114723022acd9ecb9986ef0aba4
SergeM <sbmatyunin@gmail.com>
2020-11-02 09:46:37 +0100

    Bump version: 0.1.3 → 0.2.0

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/serge-m/pytorch_nn_tools',
-    version='0.1.3',
+    version='0.2.0',
     zip_safe=False,
 )
